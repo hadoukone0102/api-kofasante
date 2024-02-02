e480da28fd97bd20f08939e16e834b7584f9c761
KONE HADOU <hadoukone0102@gmail.com>
2024-02-03 00:02:43 +0100

    creation admin

--- app/Http/Controllers/Api/AdministrateurController.php
+++ app/Http/Controllers/Api/AdministrateurController.php
@@ -24,9 +24,9 @@ class AdministrateurController extends Controller
         ]);
 
         // Check if the request is authorized to create a "Super Admin"
-        // if ($request->type === 'Super' && !$this->isAuthorizedToCreateSuperAdmin()) {
-        //     return response()->json(['message' => 'Vous n\'êtes pas autorisé à créer un Super Admin.'], 403);
-        // }
+        if ($request->type === 'Super' && !$this->isAuthorizedToCreateSuperAdmin()) {
+            return response()->json(['message' => 'Vous n\'êtes pas autorisé à créer un Super Admin.'], 403);
+        }
 
         // Check if an administrator with the same contact already exists
         if (administrateur::where('contact', $request->contact)->exists()) {
