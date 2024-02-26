05a78adf0c135eaefb9e8f1e6f5937b06a3b44cc
KONE HADOU <hadoukone0102@gmail.com>
2024-02-26 01:42:05 +0100

    merci seigneur

--- app/Http/Controllers/Api/ForgotPasswordController.php
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -11,104 +11,104 @@ class ForgotPasswordController extends Controller
 {
 
 public function ForgotPassword(Request $request)
-    {
+{
 
-        $request->validate([
-            'contact' => 'required|max:10',
+    $request->validate([
+        'contact' => 'required|max:10',
+    ]);
+
+    $Admin = Utilisateur::where('contact', $request->contact)->first();
+    if (!$Admin) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Votre numéro est inconnu'
         ]);
+    }
 
-        //$contactAdmin = $request->contactAdmin;
-        // Check if the phone number is present in the database
-        $Admin = Utilisateur::where('contact', $request->contact)->first();
-        if (!$Admin) {
+    $verificationCode = mt_rand(1000, 9999);
+    $number = $Admin->contact;
+    $name = $Admin->nomAdmin;
+    $contact = "225$number";
+
+    $code = codePassword::where('code', $verificationCode)->first();
+
+    $verifyIsOk = codePassword::firstOrCreate(
+        [
+            'contact' => $contact,
+            'code' => $verificationCode,
+        ]
+    );
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://vavasms.com/api/v1/text/single",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "username=ibnmassoudkouakou@gmail.com&password=imRu@KZfF@JB5SM&sender_id=KOFASANTE&phone=$contact&message=Votre code de validation est: $verificationCode",
+        CURLOPT_HTTPHEADER => array(
+            "Accept: */*",
+            "Content-Type: application/x-www-form-urlencoded",
+            "Host: vavasms.com"
+        ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    $responseData = json_decode($response, true);
+
+    if ($err) {
+        return response()->json([
+            'success' => false,
+            'message' => "Erreur cURL : $err"
+        ]);
+    } else {
+        if ($responseData['code'] == 0) {
+            if (($code->code) == Null) {
+                $code->code = $verificationCode;
+                $code->save();
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Le code a été envoyé avec succès',
+                    'code' => [
+                        'code' => $verificationCode,
+                        'contact' => $contact,
+                        'message' => 'Le code a été envoyé avec succès'
+                    ]
+                ]);
+            } else {
+                $code->code = Null;
+                $code->save();
+                $code->code = $verificationCode;
+                $code->save();
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Le code de confirmation a été bien envoyé',
+                    'code' => [
+                        'code' => $verificationCode,
+                        'contact' => $contact,
+                        'message' => 'Le code de confirmation a été bien envoyé'
+                    ]
+                ]);
+            }
+        } elseif ($responseData['code'] == 903) {
             return response()->json([
                 'success' => false,
-                'message' => 'votre numero est inconnu'
-            ],);
-        }
-        // Generate a random confirmation code
-        // $verificationCode = rand(100000, 999999);
-        $verificationCode = mt_rand(1000, 9999);
-        $number = $Admin->contact;
-        $name = $Admin->nomAdmin;
-        $contact = "225$number";
-
-        $code = codePassword::where('code', $verificationCode)->first();
-
-        $verifyIsOk = codePassword::firstOrCreate(
-            [
-                'contact' => $contact,
-                'code' => $verificationCode,
-            ]
-        );
-
-        // Send the request to the VavaSMS API
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://vavasms.com/api/v1/text/single",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "username=ibnmassoudkouakou@gmail.com&password=imRu@KZfF@JB5SM&sender_id=KOFASANTE&phone=$contact&message=Votre code de validation est: $verificationCode",
-            CURLOPT_HTTPHEADER => array(
-                "Accept: */*",
-                "Content-Type: application/x-www-form-urlencoded",
-                "Host: vavasms.com"
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        $responseData=json_decode($response,true);//convertir lea response json
-        if ($err) {
-             echo "cURL Error #:" . $err;
-
+                'message' => 'Le format ou le numéro est incorrect'
+            ]);
         } else {
-            if($responseData['code'] == 0) {
-                    // Checks whether the column is empty or not"
-                if (($code->code) == Null) {
-                    // Inserts the new "$verification" value in the "codeConfirm" column
-                    $code->code = $verificationCode;
-                    $code->save();
-                    return response()->json(
-                        [
-                            'success' => true,
-                            'message' => 'le code a ete envoyer avec suces',
-                            'code'=>$verificationCode
-                        ],
-                        200
-                    );
-                } else {
-                    $code->code = Null;
-                    $code->save();
-                    // Inserts the new "$verification" value in the "codeConfirm" column
-                    $code->code = $verificationCode;
-                    $code->save();
-                    return response()->json(
-                        [
-                            'success' => true,
-                            'message' => 'le code de confirmation a ete bien envoyer',
-                            'code'=>$verificationCode
-                        ],
-                        200
-                    );
-                }
-
-            }elseif($responseData['code'] == 903){
-                    return response()->json([
-                        'success' => false,
-                        'message' => 'le format ou numero  incorrecte '
-                    ],);
-            }else{
-                return response()->json([
-                    'success' => false,
-                    'message' => "erreur de serveur",
-                    'code'=>$verificationCode
-                ],);
-            }
-
+            return response()->json([
+                'success' => false,
+                'message' => "Erreur de serveur",
+                'code' => [
+                    'code' => $verificationCode,
+                    'contact' => $contact,
+                    'message' => "Erreur de serveur"
+                ]
+            ]);
         }
     }
+}
 
 
     /**
