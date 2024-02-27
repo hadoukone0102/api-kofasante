e2a764830f76f6d50b1900af40268ab0bfbc13bb
KONE HADOU <hadoukone0102@gmail.com>
2024-02-27 22:07:44 +0100

    avant dernier

--- database/migrations/2024_01_16_155137_create_documents_table.php
+++ database/migrations/2024_01_16_155137_create_documents_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     */
+    */
     public function up(): void
     {
         Schema::create('documents', function (Blueprint $table) {
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->dateTime('dateRdv');
             $table->string('typeServices')->nullable();
             $table->string('consultVar')->nullable();
-            $table->string('prog')->nullable();
+            $table->dateTime('prog')->nullable();
             $table->string('details');
             $table->string('type');
             $table->integer('couts');
